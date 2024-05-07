cb223cd5d9ec9dbee82c59ba443be1346dd14a76
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-05-07 16:50:26 +0200

    Normed draw and some

--- srcs/draw/floor_and_ceiling.c
+++ srcs/draw/floor_and_ceiling.c
@@ -1,13 +1,15 @@
 #include "cub3d.h"
 
 // inline void	clean_screen(t_c3_env *env)
-static void	draw_ceiling(t_c3_env *env)
+static inline void	draw_ceiling(t_c3_env *env)
 {
 	register int			cursor;
 	static const int		max = (HEIGHT * (WIDTH >> 2)) >> 1;
 	__int128_t				*img_ptr;
-	const __int128_t		color = ((__int128_t)env->scene.ceiling << 96) + ((__int128_t)env->scene.ceiling << 64) + \
-								((__int128_t)env->scene.ceiling << 32) + ((__int128_t)env->scene.ceiling);
+	const __int128_t		color = ((__int128_t)env->scene.ceiling << 96) + \
+								((__int128_t)env->scene.ceiling << 64) + \
+								((__int128_t)env->scene.ceiling << 32) + \
+								((__int128_t)env->scene.ceiling);
 
 	img_ptr = (__int128_t *)env->img.addr;
 	cursor = -1;
@@ -21,13 +23,15 @@ static void	draw_ceiling(t_c3_env *env)
 }
 
 // inline void	clean_screen(t_c3_env *env)
-static void	draw_floor(t_c3_env *env)
+static inline void	draw_floor(t_c3_env *env)
 {
 	register int			cursor;
 	static const int		max = (HEIGHT * (WIDTH >> 2));
 	__int128_t				*img_ptr;
-	const __int128_t		color = ((__int128_t)env->scene.floor << 96) + ((__int128_t)env->scene.floor << 64) + \
-								((__int128_t)env->scene.floor << 32) + ((__int128_t)env->scene.floor);
+	const __int128_t		color = ((__int128_t)env->scene.floor << 96) + \
+								((__int128_t)env->scene.floor << 64) + \
+								((__int128_t)env->scene.floor << 32) + \
+								((__int128_t)env->scene.floor);
 
 	img_ptr = (__int128_t *)env->img.addr;
 	cursor = ((HEIGHT * (WIDTH >> 2)) >> 1) - 1;
--- srcs/render.c
+++ srcs/render.c
@@ -4,17 +4,9 @@
 
 int	render(t_c3_env *env)
 {
-	// printf("render\n");
-	// clean_screen(env);
 	floor_and_ceiling(env);
-
 	update_player(env);
-
 	simple_raycasting(env);
-
-	draw_minimap(env);
-
 	mlx_put_image_to_window(env->mlx, env->win, env->img.img, 0, 0);
-	// mlx_put_image_to_window(env->mlx, env->win, env->scene.texture->ptr, 0, 0);
 	return (0);
 }
