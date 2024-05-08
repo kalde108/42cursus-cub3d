acd5871f97ba0cc974e993727a18e23589b30fab
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-05-08 19:25:40 +0200

    Cleanup mandatory header files

--- srcs_bonus/get_path.c
+++ srcs_bonus/get_path.c
@@ -1,31 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   player.h                                           :+:      :+:    :+:   */
+/*   get_path.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/07 22:51:56 by ibertran          #+#    #+#             */
-/*   Updated: 2024/05/08 19:24:09 by ibertran         ###   ########lyon.fr   */
+/*   Created: 2024/05/07 23:11:49 by ibertran          #+#    #+#             */
+/*   Updated: 2024/05/07 23:18:58 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PLAYER_H
-# define PLAYER_H
+#include "libft.h"
+#include "pathfinding.h"
 
-# include "ft_math.h"
-
-# define PLAYER_MOVEMENT_SPEED 0.03
-# define PLAYER_ROTATION_SPEED 0.01
-# define PLAYER_SIZE 0.1
-
-typedef struct s_player
+t_vector pathfinding(t_coord start, t_coord target)
 {
-	t_v2d_d	pos;
-	t_v2d_d	dir;
-	t_v2d_d	plane;
-	double	mv_speed;
-	double	rt_speed;
-}	t_player;
+	t_vector	path;
 
-#endif //PLAYER_H
+
+}
+
+t_vector
