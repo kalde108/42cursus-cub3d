cee8ecce2b8dd4482aafef8c3d9d0a09f208a4e2
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-05-11 20:38:12 +0200

    Fix player starting position

--- bonus/srcs/parsing/get_entities.c
+++ bonus/srcs/parsing/get_entities.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_entities.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 13:13:57 by ibertran          #+#    #+#             */
-/*   Updated: 2024/05/11 15:15:35 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/05/11 20:37:36 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	get_entities(t_vector *map, t_c3_env *env)
 {
 	if (get_player_spawn(map, &env->player))
 		return (-1);
-	if (get_monster_spawn(map, &env->player))
+	if (get_monster_spawn(map, &env->monster))
 		return (-1);
 	return (0);
 }
