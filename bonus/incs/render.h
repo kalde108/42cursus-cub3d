dca1166edf57549302d8e6155b5001551947675b
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-05-11 16:27:09 +0200

    Add thread error management

--- bonus/incs/render.h
+++ bonus/incs/render.h
@@ -6,7 +6,7 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 22:52:43 by ibertran          #+#    #+#             */
-/*   Updated: 2024/05/09 13:42:50 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/05/11 16:19:58 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 
 void	update_player(t_c3_env *env);
 void	draw_minimap(t_c3_env *env);
-void	render_map(t_c3_env *env);
+int		render_map(t_c3_env *env);
 
 #endif
--- bonus/srcs/main.c
+++ bonus/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 22:51:49 by ibertran          #+#    #+#             */
-/*   Updated: 2024/05/11 14:17:25 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/05/11 16:12:40 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,14 @@ int	main(int ac, char **av)
 		destroy_cubenv(&env);
 		return (1);
 	}
+	dprintf(2, "CPUCORES: %d\n", CPUCORES);
+	if (pthread_mutex_init(&env.call_mutex, NULL))
+	{
+		destroy_cubenv(&env);
+		return (1);
+	}
 	mlx_loop(env.mlx);
 	destroy_cubenv(&env);
+	pthread_mutex_destroy(&env.call_mutex);
 	return (0);
 }
