10667d6e7cd4939d20c9eaf7ea937fa2b9dc06f2
ibertran <128724498+ibtrd@users.noreply.github.com>
2024-05-11 17:43:32 +0200

    Norm

--- bonus/srcs/parsing/get_cell_value.c
+++ bonus/srcs/parsing/get_cell_value.c
@@ -6,7 +6,7 @@
 /*   By: ibertran <ibertran@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 23:39:06 by ibertran          #+#    #+#             */
-/*   Updated: 2024/05/11 17:37:49 by ibertran         ###   ########lyon.fr   */
+/*   Updated: 2024/05/11 17:42:21 by ibertran         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,28 @@ static char	get_layer_cell(t_vector *map, int y, int x, int layer);
 
 int	get_cell_value(t_vector map[LAYERS_COUNT], int y, int x, short *cell)
 {
-	const char	wall_cell = get_layer_cell(map, y, x, MAP_LAYER);
-	const char	floor_cell = get_layer_cell(map, y, x, FLOOR_LAYER);
-	const char	ceiling_cell = get_layer_cell(map, y, x, CEILING_LAYER);
+	const char	wall = get_layer_cell(map, y, x, MAP_LAYER);
+	const char	floor = get_layer_cell(map, y, x, FLOOR_LAYER);
+	const char	ceiling = get_layer_cell(map, y, x, CEILING_LAYER);
 
-	if ('.' == wall_cell)
+	if ('.' == wall)
 	{
-		if (floor_cell && ceiling_cell)
+		if (floor && ceiling)
 		{
-			*cell = TYPE_FL_CE | floor_cell - 'a' | ((ceiling_cell - 'a') << CEILING_SHIFT);
+			*cell = TYPE_FL_CE | floor - 'a' | (ceiling - 'a') << CEILING_SHIFT;
 			return (0);
 		}
 		else
 			return (-1); //map invalide
 	}
-	if (wall_cell)
+	if (wall)
 	{
-		*cell = TYPE_WALL | wall_cell - 'a';
+		*cell = TYPE_WALL | wall - 'a';
 		return (0);
 	}
-	if (floor_cell && ceiling_cell)
+	if (floor && ceiling)
 	{
-		*cell = TYPE_FL_CE | floor_cell - 'a' | ((ceiling_cell - 'a') << CEILING_SHIFT);
+		*cell = TYPE_FL_CE | floor - 'a' | (ceiling - 'a') << CEILING_SHIFT;
 		return (0);
 	}
 	*cell = EMPTY_CELL;
