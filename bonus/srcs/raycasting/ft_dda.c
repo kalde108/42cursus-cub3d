810a08228b84566b2d6bfd7667472ed0dcb36e66
kalde108 <29688104+kalde108@users.noreply.github.com>
2024-06-05 14:13:09 +0200

    Clean dda

--- bonus/srcs/raycasting/ft_dda.c
+++ bonus/srcs/raycasting/ft_dda.c
@@ -6,18 +6,50 @@
 /*   By: kchillon <kchillon@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/07 22:53:20 by ibertran          #+#    #+#             */
-/*   Updated: 2024/06/05 13:33:29 by kchillon         ###   ########lyon.fr   */
+/*   Updated: 2024/06/05 14:12:17 by kchillon         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "raycasting.h"
-#include "libft.h"
 #include "tile_address.h"
 
-# include <math.h>
-# include <stdio.h>
+static int	get_hit(t_cubscene *scene, t_ray *ray, size_t pos)
+{
+	if (IS_WALL(scene->map[pos])
+		&& !IS_PORTAL(ray->cell))
+	{
+		ray->cell = scene->map[pos];
+		return (1);
+	}
+	if (IS_PORTAL(scene->map[pos]))
+	{
+		if (!IS_PORTAL(ray->cell))
+		{
+			ray->cell = scene->map[pos];
+			return (1);
+		}
+		if (scene->portals.tab[GET_PORTAL(ray->cell)].linked_portal
+			== GET_PORTAL(scene->map[pos]))
+			ray->cell = 0;
+	}
+	return (0);
+}
 
-# include "cub3d.h"
+static void	move_ray(t_ray *ray)
+{
+	if (ray->side_dist.x < ray->side_dist.y)
+	{
+		ray->side_dist.x += ray->delta_dist.x;
+		ray->map_pos.x += ray->step.x;
+		ray->side = 0;
+	}
+	else
+	{
+		ray->side_dist.y += ray->delta_dist.y;
+		ray->map_pos.y += ray->step.y;
+		ray->side = 1;
+	}
+}
 
 void	ft_dda(t_cubscene *scene, t_ray *ray)
 {
@@ -28,75 +60,23 @@ void	ft_dda(t_cubscene *scene, t_ray *ray)
 	hit = 0;
 	while (hit == 0)
 	{
-		if ((ray->map_pos.x < 0 && ray->step.x <= 0)
-			|| (ray->map_pos.y < 0 && ray->step.y <= 0)
-			|| (ray->map_pos.x >= scene->width && ray->step.x >= 0)
-			|| (ray->map_pos.y >= scene->height && ray->step.y >= 0))
-		{
-			dprintf(2, "%d ALED\n", g_debug);
-			// dprintf(2, "hit_enable = %d\n", hit_enable);
-			// dprintf(2, "\t%d map_pos out of bounds\tx: %d\ty: %d\n", g_debug, ray->map_pos.x, ray->map_pos.y);
-			// dprintf(2, "\t%d map_pos out of bounds", g_debug);
-			// dprintf(2, "\tcamera pos\tx: %f\ty: %f\n", camera->pos.x, camera->pos.y);
-			// dprintf(2, "\tcamera dir\tx: %f\ty: %f\n", camera->dir.x, camera->dir.y);
-			// dprintf(2, "camera plane\tx: %f\ty: %f\n", camera->plane.x, camera->plane.y);
-			// dprintf(2, "\tray dir\tx: %f\ty: %f\n", ray->ray_dir.x, ray->ray_dir.y);
-			// dprintf(2, "\tray map_pos\tx: %d\ty: %d\n", ray->map_pos.x, ray->map_pos.y);
-		}
-		if ((ray->map_pos.x < 0 && ray->step.x > 0)
-			|| (ray->map_pos.y < 0 && ray->step.y > 0)
-			|| (ray->map_pos.x >= scene->width && ray->step.x < 0)
-			|| (ray->map_pos.y >= scene->height && ray->step.y < 0))
-		{
-			// dprintf(2, "controlled EVA\n");
-			hit_enable = 0;
-		}
-		else
-			hit_enable = 1;
-		if (hit_enable && IS_WALL(scene->map[ray->map_pos.y * scene->width + ray->map_pos.x]))
-		{
-			if (!IS_PORTAL(ray->cell))
-			{
-				hit = 1;
-				ray->cell = scene->map[ray->map_pos.y * scene->width + ray->map_pos.x];
-			}
-		}
-		if (hit_enable && IS_PORTAL(scene->map[ray->map_pos.y * scene->width + ray->map_pos.x]))
-		{
-			if (IS_PORTAL(ray->cell))
-			{
-				if (scene->portals.tab[GET_PORTAL(ray->cell)].linked_portal == GET_PORTAL(scene->map[ray->map_pos.y * scene->width + ray->map_pos.x]))
-					ray->cell = 0;
-			}
-			else
-			{
-				hit = 1;
-				ray->cell = scene->map[ray->map_pos.y * scene->width + ray->map_pos.x];
-			}
-		}
+		hit_enable = !((ray->map_pos.x < 0 && ray->step.x > 0)
+				|| (ray->map_pos.y < 0 && ray->step.y > 0)
+				|| (ray->map_pos.x >= scene->width && ray->step.x < 0)
+				|| (ray->map_pos.y >= scene->height && ray->step.y < 0));
+		if (hit_enable)
+			hit = get_hit(scene,
+					ray,
+					ray->map_pos.y * scene->width + ray->map_pos.x);
 		if (!hit)
-		{
-			if (ray->side_dist.x < ray->side_dist.y)
-			{
-				ray->side_dist.x += ray->delta_dist.x;
-				ray->map_pos.x += ray->step.x;
-				ray->side = 0;
-			}
-			else
-			{
-				ray->side_dist.y += ray->delta_dist.y;
-				ray->map_pos.y += ray->step.y;
-				ray->side = 1;
-			}
-		}
+			move_ray(ray);
 	}
 	if (ray->side == 0)
 		ray->perp_wall_dist = ray->side_dist.x - ray->delta_dist.x;
 	else
 		ray->perp_wall_dist = ray->side_dist.y - ray->delta_dist.y;
-
-	// dprintf(2, "ray->total_perp_wall_dist: %f\n", ray->total_perp_wall_dist);
 }
+
 /*
 
 x horizontal
@@ -240,8 +220,85 @@ else if (relation == 3)
 	// right face
 }
 
+*/
 
+/*
 
+void	ft_dda(t_cubscene *scene, t_ray *ray)
+{
+	int	hit;
+	int	hit_enable;
 
+	hit_enable = 1;
+	hit = 0;
+	while (hit == 0)
+	{
+		if ((ray->map_pos.x < 0 && ray->step.x <= 0)
+			|| (ray->map_pos.y < 0 && ray->step.y <= 0)
+			|| (ray->map_pos.x >= scene->width && ray->step.x >= 0)
+			|| (ray->map_pos.y >= scene->height && ray->step.y >= 0))
+		{
+			dprintf(2, "%d ALED\n", g_debug);
+			// dprintf(2, "hit_enable = %d\n", hit_enable);
+			// dprintf(2, "\t%d map_pos out of bounds\tx: %d\ty: %d\n", g_debug, ray->map_pos.x, ray->map_pos.y);
+			// dprintf(2, "\t%d map_pos out of bounds", g_debug);
+			// dprintf(2, "\tcamera pos\tx: %f\ty: %f\n", camera->pos.x, camera->pos.y);
+			// dprintf(2, "\tcamera dir\tx: %f\ty: %f\n", camera->dir.x, camera->dir.y);
+			// dprintf(2, "camera plane\tx: %f\ty: %f\n", camera->plane.x, camera->plane.y);
+			// dprintf(2, "\tray dir\tx: %f\ty: %f\n", ray->ray_dir.x, ray->ray_dir.y);
+			// dprintf(2, "\tray map_pos\tx: %d\ty: %d\n", ray->map_pos.x, ray->map_pos.y);
+		}
+		if ((ray->map_pos.x < 0 && ray->step.x > 0)
+			|| (ray->map_pos.y < 0 && ray->step.y > 0)
+			|| (ray->map_pos.x >= scene->width && ray->step.x < 0)
+			|| (ray->map_pos.y >= scene->height && ray->step.y < 0))
+		{
+			// dprintf(2, "controlled EVA\n");
+			hit_enable = 0;
+		}
+		else
+			hit_enable = 1;
+		if (hit_enable && IS_WALL(scene->map[ray->map_pos.y * scene->width + ray->map_pos.x]))
+		{
+			if (!IS_PORTAL(ray->cell))
+			{
+				hit = 1;
+				ray->cell = scene->map[ray->map_pos.y * scene->width + ray->map_pos.x];
+			}
+		}
+		if (hit_enable && IS_PORTAL(scene->map[ray->map_pos.y * scene->width + ray->map_pos.x]))
+		{
+			if (IS_PORTAL(ray->cell))
+			{
+				if (scene->portals.tab[GET_PORTAL(ray->cell)].linked_portal == GET_PORTAL(scene->map[ray->map_pos.y * scene->width + ray->map_pos.x]))
+					ray->cell = 0;
+			}
+			else
+			{
+				hit = 1;
+				ray->cell = scene->map[ray->map_pos.y * scene->width + ray->map_pos.x];
+			}
+		}
+		if (!hit)
+		{
+			if (ray->side_dist.x < ray->side_dist.y)
+			{
+				ray->side_dist.x += ray->delta_dist.x;
+				ray->map_pos.x += ray->step.x;
+				ray->side = 0;
+			}
+			else
+			{
+				ray->side_dist.y += ray->delta_dist.y;
+				ray->map_pos.y += ray->step.y;
+				ray->side = 1;
+			}
+		}
+	}
+	if (ray->side == 0)
+		ray->perp_wall_dist = ray->side_dist.x - ray->delta_dist.x;
+	else
+		ray->perp_wall_dist = ray->side_dist.y - ray->delta_dist.y;
+}
 
 */
